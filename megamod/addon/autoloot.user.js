d2f3310848d0d2ea4f9c2af6f12b55638dd54bf9
IceHacks <38994479+IceHacks@users.noreply.github.com>
2018-06-05 17:52:53 -1000

    NEW ADDONS!!!
    It is what you ALL have been waiting for! ADDONS! Autoloot is done, more to come. Docs soon

---
+++ megamod/addon/autoloot.user.js
@@ -0,0 +1,99 @@
+// ==UserScript==
+// @name         Surviv.io Addon Pack
+// @version      1.0.0
+// @description  Autoloot v1 + more soon
+// @author       IceHacks
+// @match        http://surviv.io/
+// @grant        unsafeWindow
+// ==/UserScript==
+(function() {
+	'use strict';
+	var censor = function(censor) {
+		var i = 0;
+		return function(key, value) {
+			if (i !== 0 && typeof(censor) === 'object' && typeof(value) == 'object' && censor == value)
+				return '[Circular]';
+			if (i >= 29) // seems to be a harded maximum of 30 serialized objects?
+				return '[Unknown]';
+			++i; // so we know we aren't using the original object anymore
+			return value;
+		}
+	}
+	unsafeWindow.settings = {
+		manager: {
+			loopSpeed: 100
+		},
+		autoLoot: {
+			enabled: true,
+			params: {}
+		},
+		oneClick: {
+			enabled: true,
+			params: {}
+		},
+		aimBot: {
+			enabled: false,
+			params: {}
+		}
+	}
+	var get = function(name, param) {
+		if (param) {
+			return eval("unsafeWindow.settings." + name + "." + param);
+		} else {
+			return unsafeWindow.settings[name];
+		}
+	}
+	var set = function(name, value) {
+		if (typeof value !== "string") {
+			if (value.param) {
+				return eval("unsafeWindow.settings" + name + "." + value.param + " = " + value.value);
+			} else {
+				return eval("unsafeWindow.settings" + name + " = " + value.value);
+			}
+		} else {
+			return eval("unsafeWindow.settings" + name + " = " + value);
+		}
+	}
+	var addon = function() {
+		if (unsafeWindow.game) {
+			// Game loop
+			var game = unsafeWindow.game;
+			unsafeWindow.vars = {
+				mouseDown: false
+			}
+			setInterval(function() {
+				if (game.initialized) {
+					var me = game.activePlayer;
+					var guns = me.localData.weapons;
+					// Autoloot
+					if (get("autoLoot", "enabled")) {
+						var loot = game.lootBarn;
+						var canHaveGun = (guns[0].name == "" || guns[0].name == "");
+						game.input.bOnKeyUp.call(game.input, {
+							keyCode: 70
+						});
+						if (loot.closestLoot) {
+							// The type of loot, all I know is that a gun is 3
+							var type = loot.closestLoot.type;
+							if (type == 3 && canHaveGun) {
+								// Room for a gun and that is a gun, pick it up
+								game.input.bOnKeyDown.call(game.input, {
+									keyCode: 70
+								});
+							} else if (type != 3) {
+								// No room for a gun and not a gun, pick it up.
+								game.input.bOnKeyDown.call(game.input, {
+									keyCode: 70
+								});
+							}
+						}
+					}
+				}
+			}, unsafeWindow.settings.manager.loopSpeed);
+		} else {
+			setTimeout(addon, 500);
+			return;
+		}
+	};
+	addon();
+})();
